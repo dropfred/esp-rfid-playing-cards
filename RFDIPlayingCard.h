d70235306ff90c5af70cc4c786ffd1764f5dc181
dropfred <5340116+dropfred@users.noreply.github.com>
2021-10-26 20:07:12 +0200

    Update RFDIPlayingCard.h

--- RFDIPlayingCard.h
+++ RFDIPlayingCard.h
@@ -5,9 +5,9 @@
 
 namespace RFDIPlayingCard
 {
-    BLEUUID const UUID_SERVICE{"4fafc201-1fb5-459e-8fcc-c5c9c331914b"};
-    BLEUUID const UUID_CARD   {"beb5483e-36e1-4688-b7f5-ea07361b26a8"};
-    BLEUUID const UUID_BATTERY{"4ea60680-747d-44b6-9978-0b044da1d28e"};
+    BLEUUID const UUID_SERVICE {"4fafc201-1fb5-459e-8fcc-c5c9c331914b"};
+    BLEUUID const UUID_CARD    {"beb5483e-36e1-4688-b7f5-ea07361b26a8"};
+    BLEUUID const UUID_BATTERY {"4ea60680-747d-44b6-9978-0b044da1d28e"};
 }
 
 #endif
